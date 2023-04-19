62b57cdacdfd648af9762a668bd890531261cd40
Ryan Zelazny <RyanZelazny@gmail.com>
2023-04-18 20:15:33 -0400

    Initial context setup

---
+++ src/pages/About/AboutContent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutContent = {
+    en: {
+        paraOne: "A software developer by trade, I live in upstate New York with my wife and two dogs. I enjoy cooking, board games with friends, and the occasional trip abroad when there isn't a global pandemic."
+    },
+    es: {
+        paraOne: "A software developer by trade, I live in upstate New York with my wife and two dogs. I enjoy cooking, board games with friends, and the occasional trip abroad when there isn't a global pandemic."
+    }
+}
+
+export default AboutContent;
